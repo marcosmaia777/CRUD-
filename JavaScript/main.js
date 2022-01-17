4a18d38075a58b4158c3e0b290a07f9e08f66c24
Marcos <marcosmaiafidelis777@outlook.com>
2022-01-17 11:13:39 -0300

    vigésimo quinto commit

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,12 +2,25 @@
 
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
-
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
-document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
+const tempClient = {
+    nome: "Marcos Maia",
+    email: "marcosmaiafidelis777@outlook.com",
+    celular: "81998240579",
+    cidade: "Recife"
+}
 
+// CRUD create read update delete
+const createClient = (client) => {
+    localStorage.setItem ('db_client', JSON.stringify(client))
+}
+
+
+
+    // eventos
+    document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
