4b15c9e2858635a847d83f00ff763e7c3f6cf059
Marcos <marcosmaiafidelis777@outlook.com>
2022-01-17 22:27:08 -0300

    vigésimo oitavo commit

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -6,7 +6,7 @@ const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
 const tempClient = {
-    nome: "Marcos",
+    nome: "Alice",
     email: "marcosmaiafidelis777@outlook.com",
     celular: "81998240579",
     cidade: "Jaboatão"
@@ -15,8 +15,8 @@ const tempClient = {
 // CRUD create read update delete
 const createClient = (client) => {
     const db_client = JSON.parse(localStorage.getItem('db_client'))
-    db_client.push (client)
-    localStorage.setItem("db_client", JSON.stringify(client))
+    db_client.push(client)
+    localStorage.setItem("db_client",JSON.stringify(client))
 }
 
 // eventos
